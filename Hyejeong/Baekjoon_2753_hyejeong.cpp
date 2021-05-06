c2216a0d16464b398911abad73b14a2e4915ae6e
311516h <82697724+311516h@users.noreply.github.com>
2021-05-07 07:59:06 +0900

    Update Baekjoon_2753_hyejeong.cpp

--- Hyejeong/Baekjoon_2753_hyejeong.cpp
+++ Hyejeong/Baekjoon_2753_hyejeong.cpp
@@ -1,20 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
-	int year;
+	int year = 0;
+
 	scanf("%d", &year);
-	if (year % 4 == 0) {
-		if ((year % 100 != 0) || (year % 400 == 0)) {
-			printf("1");
-		}
-		else
-		{
-			printf("0");
-		}
+
+	if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+	{
+		printf("1");
 	}
 	else
 	{
 		printf("0");
 	}
+
 	return 0;
 }
