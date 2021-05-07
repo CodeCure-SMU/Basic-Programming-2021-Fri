269aa83422d3b8bf59160acf7ef77315f8b44497
2763bh <82493151+2763bh@users.noreply.github.com>
2021-05-07 17:26:06 +0900

    Add week 2 homeworks

---
+++ Byeonghun/Baekjoon_10869_김병훈.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int A, B;
+
+	scanf("%d %d", &A, &B);
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B);
+	printf("%d\n", A % B);
+
+	return 0;
+}
