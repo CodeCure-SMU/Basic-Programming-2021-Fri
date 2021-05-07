88249f604d860ab5417806088b8f88729fe79e7c
anyuna00 <83083290+anyuna00@users.noreply.github.com>
2021-05-07 14:54:26 +0900

    Create Baekjoon_2753_anyuna.cpp

---
+++ anyuna/Baekjoon_2753_anyuna.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+	int year = 0;
+
+	scanf("%d", &year);
+
+	if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+	{
+		printf("1");
+	}
+	else
+	{
+		printf("0");
+	}
+
+	return 0;
+}
