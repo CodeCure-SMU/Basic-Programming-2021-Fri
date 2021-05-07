b039b0a88ddfcf44027022fdc48b88f78bc33e1b
annynahyun <83200739+annynahyun@users.noreply.github.com>
2021-05-07 13:28:04 +0900

    Add files via upload

---
+++ Baekjoon_2753_kimnahyun.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) 
+{
+    int year;
+    scanf_s("%d", &year);
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)printf("1");
+    else printf("0");
+    return 0;
+}
