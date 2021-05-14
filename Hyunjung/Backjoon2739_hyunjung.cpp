9abc9eb5423cf66e9b0dbc02bd2c9360db508e5a
Heo-Hyunjung <82317735+Heo-Hyunjung@users.noreply.github.com>
2021-05-14 19:45:21 +0900

    Add files via upload

---
+++ Hyunjung/Backjoon2739_hyunjung.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int dan;
+	cin >> dan;
+
+	for (int i = 1; i < 10; i++) {
+		cout << dan << " * " << i << " = " << dan * i <<endl;
+	}
+ 
+	return 0;
+}
