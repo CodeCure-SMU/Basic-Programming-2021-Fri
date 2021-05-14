56e500466875ec50cc05959c662772f5d5f7e3b7
김정인(Jeong In Kim) <40351564+lion5232@users.noreply.github.com>
2021-05-14 18:20:03 +0900

    Add files via upload

---
+++ jeong in/baekjoon_8393_김정인.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+	int i,s,c = 0;
+	scanf("%d", &s);
+	for(i = 1; i <= s; i++)
+	{
+		c += i;
+	}
+	printf("%d", c);
+	return 0;
+}
