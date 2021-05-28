081409e90d0870670bd6744039a11085906cae2c
김정인(Jeong In Kim) <40351564+lion5232@users.noreply.github.com>
2021-05-28 18:24:18 +0900

    Update baekjoon_10809_김정인.c

--- jeong in/baekjoon_10809_김정인.c
+++ jeong in/baekjoon_10809_김정인.c
@@ -23,38 +23,4 @@ int main()
 	}
 }
 
-/*#include <stdio.h>
-#include <string.h>
-
-int main(void)
-{
-	int arr[26], i, j;
-	char S[100] ={ 0 };
-	
-	scanf("%s", S);
-	
-	for(i=0; i<26; i++)
-		arr[i]=-1;
-		
-	for(i='a'; i<='z'; i++)
-	{
-		for(j=0; j<strlen(S); j++)
-		{
-			if(i==S[j])
-			{
-				arr[S[j]-'a'] = j;
-				break;
-			}
-		}
-	}
-	
-	for(i=0; i<26; i++)
-	{
-		printf("%d", arr[i]);
-		if(i<25)
-			printf(" ");
-	}
-	return 0;
-}
-*/
 
